fb171b8d83681514e2cd16631409742372c59b39
AndreFerrero <andrea.ferrero101201@gmail.com>
2025-12-19 16:33:16 +0100

    various add ons

--- sims/estim/joint/SBI/semibsl_test.r
+++ sims/estim/joint/SBI/semibsl_test.r
@@ -11,9 +11,9 @@ source("libs/models/margins/lognormal.R")
 
 # Load builders
 source("libs/models/builders/simulator.R")
-source("libs/models/builders/logposterior.R")
-source("libs/models/builders/semibsl_logposterior.R")
-
+source("libs/models/builders/bsl_logposterior.R")
+source("libs/models/builders/synthetic_semibsl.r")
+source("libs/models/builders/synthetic_gaussian_bsl.r")
 
 # Load MCMC machinery
 source("libs/mcmc/run_chain.R")
@@ -22,6 +22,8 @@ source("libs/mcmc/proposals/gaussian_rw.R")
 source("libs/mcmc/adaptation/none.R")
 source("libs/mcmc/adaptation/haario.R")
 source("libs/mcmc/adaptation/robbins_monro.R")
+source("libs/mcmc/adaptation/hybrid_haario_rm.R")
+
 # =============================================================================
 # 1. Simulate data using the simulator
 # =============================================================================
@@ -51,72 +53,117 @@ g_inv <- function(phi) {
   param
 }
 
-fn_sum_stats <- function(x) {
-    m <- median(x)
-    md <- mad(x)
-    if (md == 0) md <- 1e-6
-
-    # Standardized Max (Skewed statistic)
-    s_max <- (max(x) - m) / md
-    return(c(m, md, s_max))
+med_mad_max <- function(x) {
+  m <- median(x)
+  md <- mad(x)
+  if (md == 0) md <- 1e-6
+  smax <- (max(x) - m) / md
+  raw_max <- max(x)
+  c(m, md, raw_max)
 }
 
 log_jacobian <- function(phi) phi[2] + phi[3]
 
-logpost <- build_semibsl_logposterior(
-  simulator = fn_sim_gumbel,
-  sum_stats = fn_sum_stats,
-  n_sim = 200,
-  log_prior = fn_log_prior,
-  transform = g,
+logpost <- build_bsl_logposterior(
+  copula = copula_gumbel, margin = margin_lognormal,
+  param_map = param_map,
+  data = X,
+  simulator = simulator,
+  sum_stats = med_mad_max,
+  n_sim = 150,
+  synthetic_loglik = synthetic_semibsl,
   inverse_transform = g_inv,
-  log_jacobian = log_jacobian,
-  data = X
+  log_jacobian = log_jacobian
 )
 
 # =============================================================================
 # 3. Define proposal and run chain
 # =============================================================================
 phi_init <- g(c(mu = 0, sigma = 1, theta = 2))
-proposal <- proposal_gaussian_rw(Sigma = diag(0.01, 3))
+p <- 3
+Sigma0 <- matrix(0.1, p, p)
+diag(Sigma0) <- 1
+proposal <- proposal_gaussian_rw(Sigma0 = Sigma0)
 
 res <- run_chain(
   log_target = logpost,
   init = phi_init,
   n_iter = 10000,
   proposal = proposal,
-  adapt = adapt_robbins_monro(target_accept = 0.234)
+  adapt = adapt_haario()
 )
 
+sbi_dir <- here("sims", "estim", "joint", "SBI")
+sbi_res_dir <- here(sbi_dir, "res")
+
+# save(res, file = here(sbi_res_dir, "semibsl_10kruns_150sims_1kobs_adapthaario.Rdata"))
+load(here(sbi_res_dir, "semibsl_10kruns_150sims_1kobs_adapthaario.Rdata"))
+
 # Convert samples back to natural space
-samples_natural <- t(apply(res$samples, 1, from_unconstrained))
+samples_natural <- t(apply(res$samples, 1, g_inv))
+
+mcmc_samples <- mcmc(samples_natural)
+
+burn_in <- nrow(samples_natural)/2
+# burn_in <- 0
+mcmc_clean <- window(mcmc_samples, start = burn_in + 1, thin = 1)
 
 # =============================================================================
 # 4. Quick summaries
 # =============================================================================
+res$conv
+
 cat("Acceptance rate:", res$accept_rate, "\n")
 cat("Posterior means:\n")
-print(colMeans(samples_natural[10000:20000, ]))
+print(colMeans(as.matrix(mcmc_clean)))
 
+effectiveSize(mcmc_clean)
 # Traceplot
 
 # Arrange 1 row and 3 columns
-par(mfrow = c(1, 3), mar = c(4, 4, 2, 1)) # smaller margins for tighter plots
+par(mfrow = c(1, 3), mar = c(4, 4, 2, 1))  # smaller margins for tighter plots
 
-plot(samples_natural[, "mu"],
-  type = "l",
-  ylab = "mu", xlab = "Iteration", main = "Trace of mu"
-)
+plot(as.matrix(mcmc_clean[, "mu"]), type = "l",
+     ylab = "mu", xlab = "Iteration", main = "Trace of mu")
 
-plot(samples_natural[, "sigma"],
-  type = "l",
-  ylab = "sigma", xlab = "Iteration", main = "Trace of sigma"
-)
+plot(as.matrix(mcmc_clean[, "sigma"]), type = "l",
+     ylab = "sigma", xlab = "Iteration", main = "Trace of sigma")
 
-plot(samples_natural[, "theta"],
-  type = "l",
-  ylab = "theta", xlab = "Iteration", main = "Trace of theta"
-)
+plot(as.matrix(mcmc_clean[, "theta"]), type = "l",
+     ylab = "theta", xlab = "Iteration", main = "Trace of theta")
 
 # Reset par
+par(mfrow = c(1,1))
+
+pairs(as.matrix(mcmc_clean))
+
+acf(as.matrix(mcmc_clean))
+
+# Densities
+par(mfrow = c(1, 3))
+
+mu_dens_est <- density(as.matrix(mcmc_clean)[, "mu"])
+plot(mu_dens_est,
+    main = "Posterior Density of Mu",
+    lwd = 2, col = "blue", xlab = expression(mu)
+)
+abline(v = true_param[1], col = "red", lwd = 2, lty = 2)
+legend("topright", c("Posterior", "True"), col = c("blue", "red"), lty = 1:2)
+
+sigma_dens_est <- density(as.matrix(mcmc_clean)[, "sigma"])
+plot(sigma_dens_est,
+    main = "Posterior Density of Sigma",
+    lwd = 2, col = "blue", xlab = expression(sigma)
+)
+abline(v = true_param[2], col = "red", lwd = 2, lty = 2)
+legend("topright", c("Posterior", "True"), col = c("blue", "red"), lty = 1:2)
+
+theta_dens_est <- density(as.matrix(mcmc_clean)[, "theta"])
+plot(theta_dens_est,
+    main = "Posterior Density of Theta",
+    lwd = 2, col = "blue", xlab = expression(theta)
+)
+abline(v = true_param[3], col = "red", lwd = 2, lty = 2)
+legend("topright", c("Posterior", "True"), col = c("blue", "red"), lty = 1:2)
+
 par(mfrow = c(1, 1))
